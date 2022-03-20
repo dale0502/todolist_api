bb7627526ab898dbfb85aa7ebe970e62eda3dc4f
dale <dale.hsu@dunqian.tw>
2022-03-20 16:09:28 +0800

    todolist

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+import HelloWorld from './components/app'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faPlusSquare, faTrash)
+
+const app = createApp({})
+
+app.component('hello-world', HelloWorld)
+
+/* add font awesome icon component */
+app.component("font-awesome-icon", FontAwesomeIcon)
+
+app.mount('#app')
+
